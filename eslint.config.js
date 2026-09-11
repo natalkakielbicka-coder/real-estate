93ff4fd9dd73ee4da0bf2a3fc30d970b78ff2a66
nkpiksalia <natalkakielbicka@gmail.com>
2026-09-11 13:02:29 +0200

    chore: add ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['dist/**']
+  },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.{js,vue}'],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  eslintConfigPrettier
+]
